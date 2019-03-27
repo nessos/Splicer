5e8c0eddffe862038473354497e821efcfa37e1b
Nick Palladinos <nick.palladinos@gmail.com>
2019-03-27 17:40:28 +0200

    Add Pow example.

--- samples/Combinators/Program.cs
+++ samples/Combinators/Program.cs
@@ -1,19 +1,38 @@
 using System;
+using System.Linq.Expressions;
 using Nessos.Expressions.Splicer;
 
 namespace Combinators
 {
 	class Program
 	{
-		static void Main(string[] args)
+		
+		static Expression<Func<int, int>> Pow(int n)
 		{
-			var ors = 
+			if (n <= 0)
+				return _ => 1;
+			else
+			{
+				var f = Pow(n - 1);
+				return x => x * f.Invoke(x);
+			}
+		}
+
+		public void OrExample()
+		{
+			var ors =
 				Expr.False<string>()
 					.Or(x => x.Length > 10)
 					.Or(x => x.ToUpper() == "Hello")
 					.Splice();
-
 			Console.WriteLine(ors);
+		}
+
+		static void Main(string[] args)
+		{
+
+			Console.WriteLine(Pow(3).Splice());
+			
 			
 		}
 	}
